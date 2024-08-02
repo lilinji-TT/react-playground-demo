e79c3cb261d7a9e35c893cf2faacac0c79450f1b
lilinji-TT <112566451+lilinji-TT@users.noreply.github.com>
2024-08-02 07:54:54 -0700

    实现生成插值解析函数

--- src/ReactPlayGround/components/CodeEditor/FileNameList/FileNameItem.tsx
+++ src/ReactPlayGround/components/CodeEditor/FileNameList/FileNameItem.tsx
@@ -1,18 +1,56 @@
 import classnames from "classnames";
-import React, { useState } from "react";
+import React, { MouseEventHandler, useEffect, useRef, useState } from "react";
 
 import styles from "./index.module.scss";
 
 export interface FileNameItemProps {
   value: string;
   actived: boolean;
+  creating: boolean;
+  readonly: boolean;
+  onEditComplete: (name: string) => void;
+  onRemove: MouseEventHandler;
   onClick: () => void;
 }
 
 export const FileNameItem: React.FC<FileNameItemProps> = (props) => {
-  const { value, actived = false, onClick } = props;
+  const {
+    value,
+    creating,
+    actived = false,
+    readonly,
+    onClick,
+    onEditComplete,
+    onRemove,
+  } = props;
 
   const [name, setName] = useState(value);
+  const [editing, setEditing] = useState(creating);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const timer = useRef<number>(0);
+
+  const handleDoubleClick = () => {
+    setEditing(true);
+    timer.current = setTimeout(() => {
+      inputRef.current?.focus();
+    }, 0);
+  };
+
+  const handleInputBlur = () => {
+    setEditing(false);
+    onEditComplete(name);
+  };
+
+  useEffect(() => {
+    if (creating) {
+      inputRef?.current?.focus();
+    }
+
+    // clear timer effect
+    return () => {
+      clearTimeout(timer.current);
+    };
+  }, [creating]);
 
   return (
     <div
@@ -22,7 +60,29 @@ export const FileNameItem: React.FC<FileNameItemProps> = (props) => {
       )}
       onClick={onClick}
     >
-      <span>{name}</span>
+      {editing ? (
+        <input
+          ref={inputRef}
+          className={styles["tabs-item-input"]}
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          onBlur={handleInputBlur}
+        />
+      ) : (
+        <>
+          <span onDoubleClick={!readonly ? handleDoubleClick : () => {}}>
+            {name}
+          </span>
+          {!readonly ? (
+            <span style={{ marginLeft: 5, display: "flex" }} onClick={onRemove}>
+              <svg width="12" height="12" viewBox="0 0 24 24">
+                <line stroke="#999" x1="18" y1="6" x2="6" y2="18"></line>
+                <line stroke="#999" x1="6" y1="6" x2="18" y2="18"></line>
+              </svg>
+            </span>
+          ) : null}
+        </>
+      )}
     </div>
   );
 };
--- src/ReactPlayGround/components/CodeEditor/FileNameList/index.tsx
+++ src/ReactPlayGround/components/CodeEditor/FileNameList/index.tsx
@@ -1,6 +1,11 @@
 import { useContext, useEffect, useState } from "react";
 import { PlaygroundContext } from "../../../PlaygroundContext";
 
+import {
+  APP_COMPONENT_FILE_NAME,
+  ENTRY_FILE_NAME,
+  IMPORT_MAP_FILE_NAME,
+} from "../../../files";
 import { FileNameItem } from "./FileNameItem";
 import styles from "./index.module.scss";
 
@@ -15,21 +20,59 @@ export default function FileNameList() {
   } = useContext(PlaygroundContext);
 
   const [tabs, setTabs] = useState([""]);
+  const [creating, setCreating] = useState(false);
 
   useEffect(() => {
     setTabs(Object.keys(files));
   }, [files]);
 
+  const handleEditComplete = (name: string, prevName: string) => {
+    updateFileName(prevName, name);
+    setSelectedFileName(name);
+    setCreating(true);
+  };
+
+  const addTab = () => {
+    addFile("Comp" + Math.random().toString().slice(2, 8) + ".tsx");
+    setCreating(true);
+  };
+
+  const handleRemove = (name: string) => {
+    removeFile(name);
+    setSelectedFileName(ENTRY_FILE_NAME);
+  };
+
+  const readonlyFileNames = [
+    ENTRY_FILE_NAME,
+    IMPORT_MAP_FILE_NAME,
+    APP_COMPONENT_FILE_NAME,
+  ];
+
   return (
     <div className={styles.tabs}>
-      {tabs.map((item, index) => (
+      {tabs.map((item, index, arr) => (
         <FileNameItem
           key={item + index}
           value={item}
+          readonly={readonlyFileNames.includes(item)}
           actived={selectedFileName === item}
+          creating={creating && index === arr.length - 1}
           onClick={() => setSelectedFileName(item)}
+          onRemove={(e) => {
+            e.stopPropagation(); // prevent
+            handleRemove(item);
+          }}
+          onEditComplete={(name: string) => handleEditComplete(name, item)}
         ></FileNameItem>
       ))}
+
+      <div
+        className={styles.add}
+        style={{ cursor: "pointer" }}
+        onClick={addTab}
+      >
+        +
+      </div>
     </div>
   );
 }
